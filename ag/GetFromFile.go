694301637912ac8e5c620de54a85e7b5931be206
zen <zhangyiming748@gmail.com>
2024-06-05 09:03:49 +0800

    save

--- ag/GetFromFile.go
+++ ag/GetFromFile.go
@@ -8,7 +8,7 @@ import (
 )
 
 const (
-	host = "https://www.asmrgay.com"
+	host = "https://asmr.121231234.xyz"
 )
 
 func GetFromFile() {
@@ -18,11 +18,17 @@ func GetFromFile() {
 	}
 	root := soup.HTMLParse(string(open))
 	as := root.FindAll("a")
+	file, err := os.OpenFile("第二季601-700.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+	if err != nil {
+		return
+	}
+	defer file.Close()
 	for _, a := range as {
 		href := a.Attrs()["href"]
 		if strings.HasPrefix(href, "/asmr/中文音声") {
 			href = strings.Join([]string{host, href}, "")
 			fmt.Println(href)
+			file.WriteString(fmt.Sprintf("%s\n", href))
 		}
 
 	}
